f343455a917461496eb196a059d51cfb82776db4
Brad-Edwards <j.bradley.edwards@gmail.com>
2022-04-21 20:13:58 -0700

    Initial commit

---
+++ astral_ark/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from astral_ark.users.models import User
+from astral_ark.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
